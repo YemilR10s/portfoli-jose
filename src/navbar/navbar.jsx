4ab5155b00febf2c25fd8e7a7c8551779f078951
YemilR10s <sigifredoyemil14@gmail.com>
2024-05-19 13:58:15 -0500

    navbar desk and mobile

---
+++ src/navbar/navbar.jsx
@@ -0,0 +1,42 @@
+import "./navbar.css"
+import logo from "./logoJose.png"
+import { useState } from "react"
+
+export const Navbar = () => {
+    const [show, setShow] = useState(true)
+    const showToggle = ()=>{
+        setShow(!show)
+    }
+  return (
+    <>
+    
+    <div className="nav-container">
+
+     <div className="nav-content">
+
+        <div className="logo">
+            <img src={logo} />
+            <span>JOSE</span>
+        </div>
+        <ul className={show ? "ul-items": "ul-items active"}>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li> 
+            <select className="languaje-buton">
+            <option type='button'>Es</option>
+            <option type='button'>en</option>
+            </select>
+        </ul>
+
+        <div onClick={showToggle} className= {show ? "fa-solid fa-bars  " :"fa-solid fa-xmark"}>
+
+        </div>
+     </div>
+
+    </div>
+    
+    </>
+  )
+}
